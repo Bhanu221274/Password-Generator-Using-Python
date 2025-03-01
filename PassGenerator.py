1b30c5f5d9ca5c616cc41e886c21796491cc3c39
Bhanu221274 <kasimsettibhanukiran@gmail.com>
2025-03-01 18:40:52 +0530

    Updated the pass generator

--- PassGenerator.py
+++ PassGenerator.py
@@ -5,9 +5,9 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the Password Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like in your password?\n"))
-nr_numbers = int(input(f"How many numbers would you like in your password??\n"))
+nr_letters = int(input("No of many letters would you like in your password?\n"))
+nr_symbols = int(input("No of many symbols would you like in your password?\n"))
+nr_numbers = int(input("No of many numbers would you like in your password??\n"))
 password=[]
 for i in range(1,nr_letters+1):
     password.append(random.choice(letters))
